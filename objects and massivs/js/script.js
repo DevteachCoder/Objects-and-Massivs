26f3f2945e0c863f4c28f9577891cdb570ac676a
Doniyorbek <142497625+DevteachCoder@users.noreply.github.com>
2026-02-03 03:01:03 +0500

    Add files via upload

---
+++ objects and massivs/js/script.js
@@ -0,0 +1,54 @@
+// let name = "Doniyor"
+// let age = 22;
+// let email = "@doniyorkuziyev.com"
+
+
+// OBJECTS
+
+// let user = {
+//     name:"Doniyor",
+//     age: 24,
+//     email: "@doniyorkuziyev.com"
+// }
+// console.log(user.age); 
+
+
+
+// MASSIVES
+
+// let a = [4,56,65,12,3,68];
+// console.log(a[4]);
+
+// let a = [4,56,65,12,3,68];
+// let k = a.length;
+// console.log(k);
+
+// a[10] = 22;
+
+// console.log(a.length);
+
+
+// let cities = ["Bukhoro","Karshi","Toshkent","Andijon","Khiva","Fargana"];
+// for(let i = 0; i < cities.length; i++) {
+//     document.getElementById("demo").innerHTML += cities[i] + "<br>"
+// }
+
+
+
+// POP/Push,   SHIFT/UNSHIFT!!!!
+// let a = [
+//     "1",
+//     "2",
+//     "3",
+//     "4"
+// ];
+
+// // a.pop();     oxiridan olib tashledi!
+// // a.push("5");    oxiriga qushadi  
+// // a.shift();    boshidan olib tashledi
+// // a.unshift("0")    boshidan qushadi
+// console.log(a);
+
+
+let k = Array.from("foo");   //AJRATIB MASSIV QILIB BERADI === Array.from
+console.log(k);
